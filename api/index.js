72417864b98605677609495cb213d3f78abf0089
rydddeal <bayq80@gmail.com>
2025-02-01 15:45:23 -0500

    https://github.com/Rydddeal/HNG12API.git

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const cors = require ('cors');
+
+const app = express();
+app.use(cors()); 
+
+app.get('/', (req, res)=> {
+    const data = {
+      email: "bayq80@gmail.com",
+      current_datetime: new Date().toISOString(),
+      github_url: "https://github.com/Rydddeal/HNG12API.git",
+    }; 
+    res.status(200).json(data);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
